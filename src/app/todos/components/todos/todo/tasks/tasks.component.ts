bf51eed97632cef927bd4c9f81a825e38944a964
nazarPtyt <ockap90@libero.it>
2023-03-14 22:37:18 +0100

    feat(app) filters works

--- src/app/todos/components/todos/todo/tasks/tasks.component.ts
+++ src/app/todos/components/todos/todo/tasks/tasks.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TasksService } from '../../../../services/tasks.service';
-import { map, Observable } from 'rxjs';
+import { combineLatest, map, Observable } from 'rxjs';
 import { Task, UpdateTaskModel } from 'src/app/todos/models/task.models';
+import { TodosService } from '../../../../services/todos.service';
+import { TaskStatusEnum } from '../../../../../core/enums/taskStatus.enum';
 
 @Component({
   selector: 'todo-tasks',
@@ -14,14 +16,26 @@ export class TasksComponent implements OnInit {
   tasks$?: Observable<Task[]>;
   taskTitle!: '';
 
-  constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService, private todosService: TodosService) {}
 
   ngOnInit(): void {
-    this.tasks$ = this.tasksService.tasks$.pipe(
-      map(tasks => {
-        return tasks[this.todoId];
+    this.tasks$ = combineLatest([this.tasksService.tasks$, this.todosService.todos$]).pipe(
+      map(res => {
+        const tasks = res[0];
+        let tasksForTodo = tasks[this.todoId];
+        const todos = res[1];
+        const activeTodo = todos.find(tl => tl.id === this.todoId);
+
+        if (activeTodo?.filter === 'completed') {
+          tasksForTodo = tasksForTodo.filter(task => task.status === TaskStatusEnum.completed);
+        }
+        if (activeTodo?.filter === 'active') {
+          tasksForTodo = tasksForTodo.filter(task => task.status === TaskStatusEnum.active);
+        }
+        return tasksForTodo;
       })
     );
+
     this.tasksService.getTasks(this.todoId);
   }
 
