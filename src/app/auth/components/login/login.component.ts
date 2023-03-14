fb9dadd76c47855c28bc2e92d84ffdf72a78076a
nazarPtyt <ockap90@libero.it>
2023-03-14 23:24:39 +0100

    feat(loginPage) alert validated data works

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'todo-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.pattern(`[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$`)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    rememberMe: new FormControl(false),
+  });
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onLoginSubmit() {
+    const value = this.loginForm.value;
+    alert(JSON.stringify(value));
+  }
+}
